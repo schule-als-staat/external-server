e892caf0d3cb22eaba55ee473344ca14b62a8a7b
RaffaelW <rfflwlf@gmail.com>
2024-02-24 22:03:13 +0100

    fixed a security gap in the header and footer

--- templates/footer.js
+++ templates/footer.js
@@ -1,9 +1,52 @@
-fetch("templates/footer.html")
-    .then((res) => res.text())
-    .then((html) => {
-        let placeholder = document.querySelector("script#footer-placeholder");
-        let temp = document.createElement("div");
-        temp.innerHTML = html;
-        let footer = temp.firstChild;
-        placeholder.parentNode.replaceChild(footer, placeholder);
-    });
+(() => {
+    const footer = `
+        <footer>
+            <div class="column">
+                <p>MAX-BORN-GYMNASIUM</p>
+                <p>Maubacher Straße 62<br />71522 Backnang</p>
+
+                <nav>
+                    <a href="impressum.html">Impressum</a>
+                    <a href="datenschutz.html">Datenschutz</a>
+                </nav>
+
+                <br />
+            </div>
+
+            <div class="column">
+                <a
+                    href="https://max-born-gymnasium.de/"
+                    target="_blank"
+                    rel="noopener"
+                    class="icon"
+                >
+                    <img src="images/mbg-icon.ico" width="32" height="32" alt="MBG" />
+                </a>
+
+                <a
+                    href="https://www.instagram.com/mbg_schulealsstaat_24/"
+                    target="_blank"
+                    rel="noopener"
+                    class="icon"
+                >
+                    <img
+                        src="images/instagram.svg"
+                        width="32"
+                        height="32"
+                        alt="Instagram"
+                    />
+                </a>
+
+                <p>Kontakt:<br /><a href="mailto:sals@mbg-bk.de">sals@mbg-bk.de</a></p>
+            </div>
+        </footer>
+    `.trim();
+
+    const placeholder = document.querySelector("script#footer-placeholder");
+
+    const temp = document.createElement("div");
+    temp.innerHTML = footer;
+    const footerNode = temp.firstChild;
+
+    placeholder.parentNode.replaceChild(footerNode, placeholder);
+})();
--- templates/header.js
+++ templates/header.js
@@ -1,22 +1,71 @@
-const selectedNavItem = document.currentScript.getAttribute("website");
-const placeholder = document.querySelector("script#header-placeholder");
+(() => {
+    const header = `
+        <header>
+            <div class="top-bar">
+                <a href="index.html">
+                    <img
+                        id="sals-logo"
+                        src="images/sals-white.svg"
+                        alt="sals-logo"
+                        width="50"
+                        height="50"
+                    />
+                </a>
 
-function htmlStringToNode(htmlString) {
-    let temp = document.createElement("div");
-    temp.innerHTML = htmlString;
-    return temp.firstChild;
-}
+                <h1 id="title">United States of Max-Born</h1>
 
-fetch("templates/header.html")
-    .then((res) => res.text())
-    .then((html) => {
-        const header = htmlStringToNode(html);
+                <a href="https://max-born-gymnasium.de/">
+                    <img
+                        id="mbg-logo"
+                        src="images/mbg-white.svg"
+                        alt="mbg-logo"
+                        height="50"
+                    />
+                </a>
+            </div>
 
-        if (selectedNavItem != null) {
-            header
-                .querySelector("a#" + selectedNavItem)
-                .classList.add("selected");
-        }
+            <div class="nav-container">
+                <div class="nav-header">
+                    <p id="menu-title">Menü</p>
+                    <img
+                        class="menu-icon"
+                        src="images/menu.svg"
+                        alt="Menü"
+                        width="40"
+                        height="40"
+                        onclick="document.querySelector('nav').classList.toggle('visible')"
+                    />
+                </div>
+                <nav>
+                    <a id="news" href="index.html">News</a>
+                    <a id="projekt" href="projekt.html">Projekt</a>
+                    <a id="termine" href="termine.html">Termine</a>
+                    <a id="verfassung" href="verfassung.html">Verfassung</a>
+                    <a id="parteien" href="parteien.html">Parteien</a>
+                    <a id="königspaar" href="königspaar.html">Königspaar</a>
+                    <a id="staatsdienst" href="staatsdienst.html">Staatsdienst</a>
+                    <a id="unternehmen" href="unternehmen.html">Unternehmen</a>
+                    <a id="unterstützung" href="unterstützung.html">Unterstützung</a>
+                    <!--<a id="parlament" href="parlament.html">Parlament</a> -->
+                </nav>
+            </div>
+        </header>
+    `.trim();
 
-        placeholder.parentNode.replaceChild(header, placeholder);
-    });
+    // get the `website` attribute from the script tag
+    const selectedNavItem = document.currentScript.getAttribute("website");
+    const placeholder = document.querySelector("script#header-placeholder");
+
+    const temp = document.createElement("div");
+    temp.innerHTML = header;
+    const headerNode = temp.firstChild;
+
+    // select the current website
+    if (selectedNavItem != null) {
+        headerNode
+            .querySelector("a#" + selectedNavItem)
+            .classList.add("selected");
+    }
+
+    placeholder.parentNode.replaceChild(headerNode, placeholder);
+})();
